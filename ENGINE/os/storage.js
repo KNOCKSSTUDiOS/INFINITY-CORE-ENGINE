62b20bbf9e05f695166e9df06915c5060d6136c7
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-14 09:37:03 -0700

    Create storage.js

---
+++ ENGINE/os/storage.js
@@ -0,0 +1,21 @@
+export const storage = {
+  save(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  load(key) {
+    const v = localStorage.getItem(key);
+    return v ? JSON.parse(v) : null;
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  },
+
+  clear() {
+    localStorage.clear();
+  },
+
+  update(delta) {
+  }
+};
