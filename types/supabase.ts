b5354caa31667186d4c7a238b32b41cace9ce3ed
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-17 17:39:34 -0700

    Create supabase.ts

---
+++ types/supabase.ts
@@ -0,0 +1,23 @@
+export type Database = {
+  public: {
+    Tables: {
+      todos: {
+        Row: {
+          id: string
+          name: string
+          created_at: string | null
+        }
+        Insert: {
+          id?: string
+          name: string
+          created_at?: string | null
+        }
+        Update: {
+          id?: string
+          name?: string
+          created_at?: string | null
+        }
+      }
+    }
+  }
+}
