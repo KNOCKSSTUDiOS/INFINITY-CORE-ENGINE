0f5d14cb0cad1bd22a2a674ef0c62d9ff2e38173
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-19 18:30:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const mobileRoute = require("./routes/mobileRoute");
-app.use("/", mobileRoute);
+// FILE: server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`INFINITY-CORE-ENGINE running at http://localhost:${PORT}`);
+});
